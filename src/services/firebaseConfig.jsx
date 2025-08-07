f65720709b975719d503feb1b511f0f1ff9400ff
mannthakur310 <139780779+mannthakur310@users.noreply.github.com>
2025-08-07 15:20:24 +0530

    modification

--- src/services/firebaseConfig.jsx
+++ src/services/firebaseConfig.jsx
@@ -20,4 +20,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
